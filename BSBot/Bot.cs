5b73eb8fefe9b4de217c468b902f354ce3f85ce2
JulianusIV <65790187+JulianusIV@users.noreply.github.com>
2021-06-04 22:00:35 +0200

    Basic Bot Logic

---
+++ BSBot/Bot.cs
@@ -0,0 +1,70 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BSBot
+{
+	public class Bot
+	{
+		#region Singleton
+		private static Bot _instance;
+		private static readonly object padlock = new object();
+		public static Bot Instance
+		{
+			get
+			{
+				lock (padlock)
+				{
+					if (_instance == null)
+						_instance = new Bot();
+					return _instance;
+				}
+			}
+		}
+		#endregion
+
+		public DiscordClient Client { get; private set; }
+
+		public CommandsNextExtension Commands { get; private set; }
+
+		public ConfigJson ConfigJson { get; set; }
+
+		public async Task RunAsync()
+		{
+			string json = File.ReadAllText("config.json");
+			ConfigJson = JsonSerializer.Deserialize<ConfigJson>(json);
+
+			DiscordConfiguration config = new DiscordConfiguration
+			{
+				Token = ConfigJson.Token,
+				TokenType = TokenType.Bot,
+				AutoReconnect = true,
+				MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug
+			};
+
+			Client = new DiscordClient(config);
+
+			//Register client events
+
+			CommandsNextConfiguration commandsConfig = new CommandsNextConfiguration
+			{
+				StringPrefixes = new string[] { ConfigJson.Prefix },
+				EnableMentionPrefix = true
+			};
+			Commands = Client.UseCommandsNext(commandsConfig);
+
+			//Register command modules
+			Commands.RegisterCommands(typeof(Bot).GetTypeInfo().Assembly);
+
+			//Register command events
+
+			await Client.ConnectAsync();
+
+			//Delay Task forever (-1) to keep program running while waiting for events
+			await Task.Delay(-1);
+		}
+	}
+}
---
+++ BSBot/ConfigJson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BSBot
+{
+	public class ConfigJson
+	{
+		public string Token { get; set; }
+		public string Prefix { get; set; }
+		public string ConnectionString { get; set; }
+	}
+}
